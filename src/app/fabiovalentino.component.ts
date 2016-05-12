e51157aa074a85819cb520d69263b9fd2564451a
Lago FVC <fabiovc27@gmail.com>
2016-05-11 18:40:04 -0700

    following the TODO tutorial in the todo/ folder from the egghead.io site

--- src/app/fabiovalentino.component.ts
+++ src/app/fabiovalentino.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { TodoItem } from './todo/todo-item'
 
 @Component({
   moduleId: module.id,
   selector: 'fabiovalentino',
   templateUrl: 'fabiovalentino.component.html',
-  styleUrls: ['fabiovalentino.component.css']
+  styleUrls: ['fabiovalentino.component.css'],
+  directives: [TodoItem]
 })
 export class FabiovalentinoAppComponent {
   title = 'f a b i o . v a l e n t i n o';
