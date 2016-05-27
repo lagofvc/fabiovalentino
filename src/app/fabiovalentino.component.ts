d47d0520032ecb6cd0236a084e6c0a599f0ac274
Lago FVC <fabiovc27@gmail.com>
2016-05-27 00:35:56 -0700

    some cosmetics

--- src/app/fabiovalentino.component.ts
+++ src/app/fabiovalentino.component.ts
@@ -9,10 +9,6 @@ import { TodoList } from './todo/todo-list';
   directives: [TodoInput, TodoList]
 })
 export class FabiovalentinoAppComponent {
-<<<<<<< HEAD
 
   public title: string = 'f a b i o . v a l e n t i n o';
-=======
-  title = `f a b i o  v a l e n t i n o`;
->>>>>>> 835bdb87a9ff60f97f8807f03e5142fee0b3e677
 }
